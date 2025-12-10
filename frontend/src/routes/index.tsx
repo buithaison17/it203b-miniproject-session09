e24bdb43415e24ff1aba7424c892a385a3a277ce
Dao Truong Son <daotruongson2006@gmail.com>
2025-12-10 22:13:47 +0700

    config routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,38 +1,3 @@
 import { createBrowserRouter } from "react-router-dom";
-import AdminManager from "../admin/components/AdminManager";
-import OrderManager from "../admin/pages/OderManager";
-import StationManagers from "../admin/pages/StationManager";
-import BusCompany from "../pages/Information/BusCompany/BusCompany";
-import Station from "../pages/Information/Station/Station";
-import Trip from "../pages/Information/Trip/Trip";
-import Layout from "../components/Layout";
 
-export const routers = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-
-    children: [
-      {
-        path: "bus-company",
-        element: <BusCompany />,
-      },
-      {
-        path: "station",
-        element: <Station />,
-      },
-      {
-        path: "trip",
-        element: <Trip />,
-      },
-    ],
-  },
-  {
-    path: "/admin",
-    element: <AdminManager />,
-    children: [
-      { path: "order", element: <OrderManager /> },
-      { path: "station", element: <StationManagers /> },
-    ],
-  },
-]);
+export const routers = createBrowserRouter([]);
