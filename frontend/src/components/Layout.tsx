179386a8163edc4a1b62414d8134f1926a0763de
Dao Truong Son <daotruongson2006@gmail.com>
2025-12-10 22:12:45 +0700

    update layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../components/header';
-import Footer from '../components/footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const Layout = (): React.ReactElement => {
   return (
     <div className="min-h-screen flex flex-col font-sans">
       <Header />
-      
-      <div className="h-[40px]"></div>
-      
+
+      <div className="h-10"></div>
+
       <main className="flex-1">
         <Outlet />
       </main>
-      
+
       <Footer />
     </div>
   );
 };
-
-
 
 export default Layout;
