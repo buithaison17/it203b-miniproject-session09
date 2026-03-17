fa59edc6375781f09928e2d33ce89b0fa2d6ac7e
Kebo Pabrireols <viet4112006@gmail.com>
2026-03-17 22:52:51 +0700

    hien thi thong ke

--- src/logger/Logger.java
+++ src/logger/Logger.java
@@ -20,7 +20,17 @@ public class Logger {
     public void log(LogLevel level, String message) {
         String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
         String thread = Thread.currentThread().getName();
-
+        if (level == LogLevel.INFO) {
+            infoCount++;
+        } else if (level == LogLevel.ERROR) {
+            errorCount++;
+        }
         System.out.println("[" + time + "] [" + level + "] [" + thread + "] " + message);
+    }
+    public synchronized void printStats() {
+        System.out.println("\n===== LOG STATS =====");
+        System.out.println("INFO  : " + infoCount);
+        System.out.println("ERROR : " + errorCount);
+        System.out.println("TOTAL : " + (infoCount + errorCount));
     }
 }
