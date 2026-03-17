7bb91f190287e179517d5e2c4a0c75a1e1d56f06
sonbui <thaisonb936@gmail.com>
2026-03-17 21:15:33 +0700

    upload code intersection

--- src/models/vehicle/Vehicle.java
+++ src/models/vehicle/Vehicle.java
@@ -1,5 +1,55 @@
 package models.vehicle;
 
-public abstract class Vehicle {
+import models.traffic.Intersection;
+import pattern.observer.Observer;
+import pattern.state.TrafficLight;
 
+public abstract class Vehicle implements Runnable, Observer {
+    private String id;
+    private String modelName;
+    private int priority;
+
+    public Vehicle(String id, String modelName, int priority) {
+        this.id = id;
+        this.modelName = modelName;
+        this.priority = priority;
+    }
+
+    abstract public void move();
+
+    abstract public void stop();
+
+    @Override
+    public void run() {
+        Intersection.getInstance().arrive(this);
+    }
+
+    @Override
+    public void update(TrafficLight light) {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
 }
