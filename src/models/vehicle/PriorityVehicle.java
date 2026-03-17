bdc7550abfbf4d2a0d8bd3a1f4202ca6c5c16823
huy <huyxauzai2601@gmail.com>
2026-03-17 22:41:18 +0700

    update Vehicle

--- src/models/vehicle/PriorityVehicle.java
+++ src/models/vehicle/PriorityVehicle.java
@@ -1,4 +1,20 @@
 package models.vehicle;
 
-public class PriorityVehicle {
+import models.traffic.Intersection;
+
+public class PriorityVehicle extends Vehicle {
+
+    public PriorityVehicle(String id, int speed, Intersection intersection) {
+        super(id, speed, 1, intersection);
+    }
+
+    @Override
+    public void move() {
+        System.out.println("[Ambulance " + id + "] PRIORITY moving!");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("[Ambulance " + id + "] IGNORE RED LIGHT");
+    }
 }
--- src/models/vehicle/StandardVehicle.java
+++ src/models/vehicle/StandardVehicle.java
@@ -1,5 +1,20 @@
 package models.vehicle;
 
-class StandardVehicle {
+import models.traffic.Intersection;
 
+public class StandardVehicle extends Vehicle {
+
+    public StandardVehicle(String id, int speed, Intersection intersection) {
+        super(id, speed, 0, intersection);
+    }
+
+    @Override
+    public void move() {
+        System.out.println("[Car " + id + "] moving at speed " + speed);
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("[Car " + id + "] STOPPED at red light");
+    }
 }
