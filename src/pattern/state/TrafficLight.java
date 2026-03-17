4fa1528f6b3b5e0b884f12f1fd4fb5a2bb8a1089
DoTriCong <tricong5526@gmail.com>
2026-03-17 20:24:33 +0700

    upload

--- src/pattern/state/TrafficLight.java
+++ src/pattern/state/TrafficLight.java
@@ -1,4 +1,22 @@
 package pattern.state;
 
 public class TrafficLight {
+
+    private TrafficLightState currentState;
+
+    public TrafficLight() {
+        currentState = new RedState(); // bắt đầu từ đèn đỏ
+    }
+
+    public void setState(TrafficLightState state) {
+        this.currentState = state;
+    }
+
+    public void change() {
+        currentState.handle(this);
+    }
+
+    public String getCurrentState() {
+        return currentState.getStateName();
+    }
 }
